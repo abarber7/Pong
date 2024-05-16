62cd57b211ce2bd6daa6c8696ca83f4c353a2a7b
Antonio D. Barber <antonio.barber@wsu.edu>
2024-05-16 11:05:07 -0700

    Created Pong Files

---
+++ main.lua
@@ -0,0 +1,46 @@
+--[[
+    Pong Remake
+
+    Originally programmed by Atari in 1972. Features two
+    paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 10 points wins.
+
+    This version is built to more closely resemble the NES than
+    the original Pong machines or the Atari 2600 in terms of
+    resolution, though in widescreen (16:9) so it looks nicer on 
+    modern systems.
+]]
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
+function love.load()
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+        -- begin rendering at virtual resolution
+        push:apply('start')
+
+        -- condensed onto one line from last example
+        -- note we are now using virtual width and height now for text placement
+        love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    
+        -- end rendering at virtual resolution
+        push:apply('end')
+end
